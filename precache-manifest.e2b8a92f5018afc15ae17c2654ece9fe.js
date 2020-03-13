fe8f210081b541ebd2b104caf88f7d26ccb210b8
thismytests <iunknowunnamed@gmail.com>
2020-03-13 13:43:24 +0200

    9cbec56

---
+++ precache-manifest.e2b8a92f5018afc15ae17c2654ece9fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96e19f61f6a91721536",
+    "url": "/videl/css/app.ae2f8a3e.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/videl/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a02908eaf159c153ce0564f124e7602f",
+    "url": "/videl/index.html"
+  },
+  {
+    "revision": "f96e19f61f6a91721536",
+    "url": "/videl/js/app.cef2ebc0.js"
+  },
+  {
+    "revision": "6b0549469d895307da00",
+    "url": "/videl/js/chunk-vendors.7dc7be23.js"
+  },
+  {
+    "revision": "287c0849d9edcca18edfbf3c1d446558",
+    "url": "/videl/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/videl/robots.txt"
+  }
+]);
